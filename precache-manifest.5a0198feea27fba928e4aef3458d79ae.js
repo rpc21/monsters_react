fbc7acf2ec8a3ab3e00091a0dad0aa3f80a340ad
Ryan Culhane <rpc21@duke.edu>
2019-07-12 21:32:09 -0400

    Updates

---
+++ precache-manifest.5a0198feea27fba928e4aef3458d79ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052349ec0bd11c24a7ced2e49733e8fe",
+    "url": "/monsters_react/index.html"
+  },
+  {
+    "revision": "52ce45aec696de24e085",
+    "url": "/monsters_react/static/css/main.ec4c8bb5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters_react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "52ce45aec696de24e085",
+    "url": "/monsters_react/static/js/main.01626bfd.chunk.js"
+  },
+  {
+    "revision": "876065c10fce63b4f022",
+    "url": "/monsters_react/static/js/runtime~main.49d4535c.js"
+  }
+]);
